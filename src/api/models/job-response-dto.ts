df5eb1db70868fc24bb99dc42574ba13f6af3d18
Konrad Bartczak <konradbartczak@onet.pl>
2020-02-08 20:02:26 +0100

    Worker name added to form show history

--- src/api/models/job-response-dto.ts
+++ src/api/models/job-response-dto.ts
@@ -1,8 +1,10 @@
 /* tslint:disable */
 import { CarServiceDto } from './car-service-dto';
 import { PartDto } from './part-dto';
+import { WorkerDto } from './worker-dto';
 export interface JobResponseDto {
   carServiceDto?: CarServiceDto;
   createdDate?: string;
   partsDto?: Array<PartDto>;
+  workerDto?: WorkerDto;
 }
--- src/app/components/logged/vehicle-history/vehicle-history.component.ts
+++ src/app/components/logged/vehicle-history/vehicle-history.component.ts
@@ -14,7 +14,7 @@ import { JobResponseDto } from 'src/api/models';
 export class VehicleHistoryComponent implements OnInit {
   @Select(state => state.job.jobHistory)
   jobHistory$: Observable<JobResponseDto[]>;
-  displayedColumns: string[] = ['Part', 'CarService', 'Date'];
+  displayedColumns: string[] = ['Part', 'CarService', 'Worker', 'Date'];
   constructor(
     public store: Store,
     public matDialogRef: MatDialogRef<VehicleDetailsComponent>,
